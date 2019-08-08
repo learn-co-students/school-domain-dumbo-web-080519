2a756d2fa1a7e408e72ed4d962ff68f71da417de
Brian Wong <bwong92@gmail.com>
2019-08-08 03:09:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School
+
+  attr_reader :name, :roster
+  def initialize(roster)
+    @roster = {}
+  end
+
+  
+
+  def add_student(name,grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    roster_hash = self.roster
+    roster_hash[grade]
+  end
+
+  def sort
+    @roster.each do |grade,students|
+      students.sort!
+    end
+  end
+end
